22250fc7c61e745074ec39b9ae2f1bb6f81dce95
macjabeth <macbethjonathan@gmail.com>
2019-03-05 22:00:29 +0000

    add action consts and handlers

---
+++ todo/src/actions/index.js
@@ -0,0 +1,20 @@
+export const UPDATE_INPUT = 'UPDATE_INPUT';
+export const updateInput = value => ({ type: UPDATE_INPUT, value });
+
+export const ADD_TODO = 'ADD_TODO';
+export const addTodo = task => ({ type: ADD_TODO, task });
+
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const toggleTodo = id => ({ type: TOGGLE_TODO, id });
+
+export const UPDATE_TODO = 'UPDATE_TODO';
+export const updateTodo = (id, task) => ({ type: UPDATE_TODO, id, task });
+
+export const DELETE_TODO = 'DELETE_TODO';
+export const deleteTodo = id => ({ type: DELETE_TODO, id });
+
+export const CLEAR_TODOS = 'CLEAR_TODOS';
+export const clearTodos = () => ({ type: CLEAR_TODOS });
+
+export const TOGGLE_SEARCH = 'TOGGLE_SEARCH';
+export const toggleSearch = () => ({ type: TOGGLE_SEARCH });
